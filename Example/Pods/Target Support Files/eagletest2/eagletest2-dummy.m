265f371bb6b5efcf7c2317d67de491ca394bb0fb
eagle <yin.yongzhen@olaola.chat>
2021-12-21 19:03:48 +0800

    eagle test

---
+++ Example/Pods/Target Support Files/eagletest2/eagletest2-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_eagletest2 : NSObject
+@end
+@implementation PodsDummy_eagletest2
+@end
---
+++ Example/Pods/Target Support Files/eagletest2/eagletest2-umbrella.h
@@ -0,0 +1,81 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "CNamaSDK.h"
+#import "FUActionRecognition.h"
+#import "FUAIConfig.h"
+#import "FUAIKit.h"
+#import "FUAISegment.h"
+#import "FUAnimation.h"
+#import "FUAnimoji.h"
+#import "FUAvatar.h"
+#import "FUAvatarColorKeys.h"
+#import "FUAvatarMakeup.h"
+#import "FUBackground.h"
+#import "FUBeauty.h"
+#import "FUBodyBeauty.h"
+#import "FUBodyBeautyDefineKeys.h"
+#import "FUCameraAnimation.h"
+#import "FUCaptureCamera.h"
+#import "FUCLIColor.h"
+#import "FUComicFilter.h"
+#import "FUConfig.h"
+#import "FUDeformationKeys.h"
+#import "FUEnum.h"
+#import "FUFacepupKeys.h"
+#import "FUFaceRectInfo.h"
+#import "FUFacialFeatures.h"
+#import "FUFilterDefineKey.h"
+#import "FUGesture.h"
+#import "FUGLContext.h"
+#import "FUGLDisplayView.h"
+#import "FUGreenScreen.h"
+#import "FUGroupAnimation.h"
+#import "FUHairBeauty.h"
+#import "FUHairDefineKey.h"
+#import "FUImageHelper.h"
+#import "FUInternalCameraSetting.h"
+#import "FUItem+createTexture.h"
+#import "FUItem.h"
+#import "FUKeysDefine.h"
+#import "FULight.h"
+#import "FULightMakeup.h"
+#import "FULightMakeUpTextureKeyDefineKey.h"
+#import "FUMakeup.h"
+#import "FUMakeUpChildStrengthDefineKey.h"
+#import "FUMakeUpColorsKey.h"
+#import "FUMakeUpTextureKey.h"
+#import "FUMakeupValueDefine.h"
+#import "FUMusicFilter.h"
+#import "FUNormalConfig.h"
+#import "FUOvonicMap.h"
+#import "FUParam.h"
+#import "FUPoster.h"
+#import "FUQualitySticker.h"
+#import "FURecordEncoder.h"
+#import "FURenderableObject.h"
+#import "FURenderer.h"
+#import "FURenderIO.h"
+#import "FURenderKit.h"
+#import "FUScene.h"
+#import "FUSceneCamera.h"
+#import "FUSetupConfig.h"
+#import "FUSticker.h"
+#import "FUStickerContainer.h"
+#import "FUStruct.h"
+#import "metamacros.h"
+#import "UIDevice+FURenderKit.h"
+#import "UIImage+FURenderKit.h"
+
+FOUNDATION_EXPORT double eagletest2VersionNumber;
+FOUNDATION_EXPORT const unsigned char eagletest2VersionString[];
+
